b69b692b0c942ee00bf8becf725f8baa448a3c0a
dhiraj61 <chaudharydhiraj715@gmail.com>
2024-03-16 19:07:17 +0530

    render html and json

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, resp) => {
-  console.log("We are getting name from browser " + req.query.name);
-  resp.send("Welcome, " + req.query.name);
+  resp.send(`
+  <h1>Welcome, This Is Homepage</h1>
+  <a href="/about">Go to About Us Page</a>
+  `);
 });
 
 app.get("/about", (req, resp) => {
-  resp.send("Welcome, " + req.query.name + " at About Us Page");
+  resp.send(`
+  <h1>Welcome, This Is About Us Page</h1>
+  <input type="text" name="name">
+  <button>Submit</button>
+  <a href="/">Go to Home Page</a>`);
 });
 
 app.get("/contactUs", (req, resp) => {
-  resp.send("Welcome, This Is Contact Us Page");
+  resp.send([
+    { name: "Dhiraj", email: "dhiraj@gmail.com" },
+    { name: "Niraj", email: "niraj@gmail.com" },
+  ]);
 });
 
 app.listen(3000);
