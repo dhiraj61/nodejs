342bc0a764210d77cd739cd40d5c54df40ded399
dhiraj61 <chaudharydhiraj715@gmail.com>
2024-03-16 18:41:15 +0530

    http in node js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-console.log("starting process");
+const http = require("http");
+const data = require("./data");
+const { json } = require("express");
 
-setTimeout(() => {
-  console.log("execute after 2 second");
-}, 2000);
-
-setTimeout(() => {
-  console.log("execute after 0 second");
-});
-
-console.log("process Completed");
+http
+  .createServer((req, resp) => {
+    resp.writeHead(200, { "Content-Type": "applicatinjson" });
+    resp.write(JSON.stringify(data));
+    resp.end();
+  })
+  .listen(3000);
