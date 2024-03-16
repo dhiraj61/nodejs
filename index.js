97bfc614f6d5769e58eaa4711a43762fdcdac50d
dhiraj61 <chaudharydhiraj715@gmail.com>
2024-03-16 16:46:56 +0530

    commandline input

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const http = require("http");
-const data = require("./data");
+const fs = require("fs");
 
-http
-  .createServer((req, resp) => {
-    resp.writeHead(200, { "Content-Type": "applicationjson" });
-    resp.write(JSON.stringify(data));
-    resp.end();
-  })
-  .listen(4000);
+const input = process.argv;
+
+if (input[2] == "add") {
+  fs.writeFileSync(input[3], input[4]);
+} else if (input[2] == "remove") {
+  fs.unlinkSync(input[3]);
+} else {
+  console.log("Invalid Input");
+}
