ba2b6a7757cfbe10f73a7f7c89c46c78b89aa170
dhiraj61 <chaudharydhiraj715@gmail.com>
2024-03-16 20:28:34 +0530

    ejs

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const path = require("path");
 const app = express();
 const pathPublic = path.join(__dirname, "public");
 
-// app.use(express.static(pathPublic));
+app.set("view engine", "ejs"); // set up ejs for templating
 
 app.get("/", (req, resp) => {
   resp.sendFile(`${pathPublic}/index.html`);
+});
+
+app.get("/profile", (req, resp) => {
+  const user = {
+    name: "Dhiraj",
+    email: "dhiraj@gmail.com",
+    city: "Surat",
+  };
+  resp.render("profile", { user });
 });
 
 app.get("/about", (req, resp) => {
