3eb36e569fcaa92c56131eecb67693655b71f833
dhiraj61 <chaudharydhiraj715@gmail.com>
2024-03-16 18:22:04 +0530

    node js work process

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-// console.log("starting process");
+console.log("starting process");
 
-// // console.log("Logical Process");
+setTimeout(() => {
+  console.log("execute after 2 second");
+}, 2000);
 
-// setTimeout(() => {
-//   console.log("Logical Process");
-// }, 2000);
-
-// console.log("Process Completed");
-
-let a = 10;
-let b = 0;
-
-console.log("a: " + a + ", b: " + b);
-
-let waitingData = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(20);
-  }, 2000);
+setTimeout(() => {
+  console.log("execute after 0 second");
 });
 
-waitingData.then((data) => {
-  b = data;
-  console.log(a + b);
-});
+console.log("process Completed");
