b6d8ba1977365c389e053bb4565c629ae9c33308
dhiraj61 <chaudharydhiraj715@gmail.com>
2024-03-16 19:40:19 +0530

    html page in express 2

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const path = require("path");
 const app = express();
 const pathPublic = path.join(__dirname, "public");
 
-app.use(express.static(pathPublic));
+// app.use(express.static(pathPublic));
 
+app.get("/", (req, resp) => {
+  resp.sendFile(`${pathPublic}/index.html`);
+});
+
+app.get("/about", (req, resp) => {
+  resp.sendFile(`${pathPublic}/About.html`);
+});
+
+app.get("*", (req, resp) => {
+  resp.sendFile(`${pathPublic}/nopage.html`);
+});
 app.listen(3000);
