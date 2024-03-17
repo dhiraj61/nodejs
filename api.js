dcf4d390d5f438a62e9a057ee45da31c6c864807
dhiraj61 <chaudharydhiraj715@gmail.com>
2024-03-17 14:51:31 +0530

    api put & delete

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const dbConnect = require("./mongodb");
 const express = require("express");
-
+const mongodb = require("mongodb");
 const app = express();
 
 app.use(express.json());
@@ -16,5 +16,19 @@ app.post("/", async (req, resp) => {
   await data.insertOne(req.body);
   data = await data.find({}).toArray();
   resp.send(data);
+});
+
+app.put("/:model", async (req, resp) => {
+  let data = await dbConnect();
+  await data.updateOne({ model: req.params.model }, { $set: req.body });
+  let result = await data.find({}).toArray();
+  resp.send(result);
+});
+
+app.delete("/:id", async (req, resp) => {
+  let data = await dbConnect();
+  await data.deleteOne({ _id:new mongodb.ObjectId(req.params.id) });
+  let result = await data.find({}).toArray();
+  resp.send(result);
 });
 app.listen(3000);
