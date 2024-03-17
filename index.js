d5f88e5c4c54812217b4294d6c2607a0979a3d03
dhiraj61 <chaudharydhiraj715@gmail.com>
2024-03-17 17:57:52 +0530

    search with mongoose

--- index.js
+++ index.js
@@ -6,30 +6,14 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/", async (req, resp) => {
-  let data = new mobiles(req.body);
-  let result = await data.save();
-  resp.send(result);
-});
-
-app.get("/", async (req, resp) => {
-  let data = await mobiles.find();
+app.get("/:key", async (req, resp) => {
+  let data = await mobiles.find({
+    $or: [
+      { model: { $regex: req.params.key } },
+      { brand: { $regex: req.params.key } },
+    ],
+  });
   resp.send(data);
-});
-
-app.put("/:_id", async (req, resp) => {
-  let data = await mobiles.updateOne(req.params, { $set: req.body });
-  resp.send("data updated");
-  if (data.acknowledged) {
-    console.log("Data updated successfully!");
-  }
-});
-
-app.delete("/:_id", async (req, resp) => {
-  let data = await mobiles.deleteOne(req.params);
-  if (data.deletedCount != 0) {
-    resp.send("Data Deleted Succefully");
-  }
 });
 
 app.listen(3000);
