e71a547fe7cd8b83800c1274a0163696398824b3
dhiraj61 <chaudharydhiraj715@gmail.com>
2024-03-17 16:02:16 +0530

    crud with mongoose

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 const mongoose = require("mongoose");
 
-const main = async () => {
-  let conn = mongoose.connect("mongodb://localhost:27017/electronic");
-  let MobileSchema = new mongoose.Schema({
-    model: String,
-    price: Number,
-    brand: String,
-  });
-  let Mobilemodel = mongoose.model("mobiles", MobileSchema);
-  let data = Mobilemodel({
-    model: "A50",
-    price: 25000,
+const conn = mongoose.connect("mongodb://localhost:27017/electronic");
+const MobileSchema = new mongoose.Schema({
+  model: String,
+  price: Number,
+  brand: String,
+});
+
+const saveInDb = async () => {
+  const Mobiles = mongoose.model("mobiles", MobileSchema);
+  let data = await Mobiles({
+    model: "A53",
+    price: 30000,
     brand: "Samsung",
   });
   let result = await data.save();
   console.log(result);
 };
 
-main();
+const updateInDb = async () => {
+  const Mobiles = mongoose.model("mobiles", MobileSchema);
+  let data = await Mobiles.updateOne(
+    { model: "A52" },
+    { $set: { price: 28500 } }
+  );
+  console.log(data);
+};
+
+const deleteInDb = async () => {
+  const Mobiles = mongoose.model("mobiles", MobileSchema);
+  let data = await Mobiles.deleteOne({ model: "A52" });
+  console.log(data);
+};
+
+const findInDb = async () => {
+  const Mobiles = mongoose.model("mobiles", MobileSchema);
+  let data = await Mobiles.find({});
+  console.log(data);
+};
+
+findInDb();
