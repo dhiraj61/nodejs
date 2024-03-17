65ea9ddf4209373e200d0c67cb3d96cb93e03ed0
dhiraj61 <chaudharydhiraj715@gmail.com>
2024-03-17 18:44:13 +0530

    os cammands

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-const express = require('express');
-const multer = require('multer');
+const os = require("os");
 
-const app = express();
-
-const upload = multer({
-  storage:multer.diskStorage({
-    destination:function(req,file,cb){
-      cb(null,"uploads");
-    },
-    filename:function(req,file,cb){
-      cb(null,file.fieldname+"-"+Date.now()+".jpg")
-    }
-  })
-}).single("user_file");
-
-app.post("/",upload,(req,resp)=>{
-  resp.send("File Uploaded");
-});
-
-app.listen(4500);
+console.log(os.arch());
+console.log(os.freemem() / (1024 * 1024 * 1024));
+console.log(os.totalmem() / (1024 * 1024 * 1024));
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.userInfo());
